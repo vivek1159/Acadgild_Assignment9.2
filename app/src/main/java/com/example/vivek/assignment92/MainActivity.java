2d3dd9b8d4640a8d8057b187c035ffa9f56381f1
vivek1159 <vivek1159@gmail.com>
2016-05-08 23:14:01 +0530

    Initial Upload.

---
+++ app/src/main/java/com/example/vivek/assignment92/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.vivek.assignment92;
+
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void onClickShowDialog (View v){
+
+        AlertDialog.Builder dialogbuilder = new AlertDialog.Builder(MainActivity.this,R.style.Theme_Dialog);
+        dialogbuilder.setView(R.layout.dialog_layout);
+
+        Dialog deleteDialog = dialogbuilder.create();
+        deleteDialog.show();
+    }
+
+    public void onClickYes (View v)
+    {
+        Toast.makeText(MainActivity.this,"YESS!!!",Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickNo (View v){
+        Toast.makeText(MainActivity.this,"NOOO!!!",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
